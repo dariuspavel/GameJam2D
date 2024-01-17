38fe937216bd34a10ee4a9281d107a5aa7c92d3c
PsyBanana <83030305+PsyBanana@users.noreply.github.com>
2024-01-17 22:41:51 +0200

    Boss Ability1
    Instantiate bullets around the player, deal damage.

---
+++ Assets/Scripts/BossFight/BossHealth.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHealth : MonoBehaviour
+{
+
+    public int health = 1000;
+  
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
