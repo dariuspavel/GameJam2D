54d7cd741371ad5d6440961b02d4238dff85a47b
Darius Pavel <darius.pvl99@gmail.com>
2024-01-19 00:19:23 +0200

    add the pause menu
    When "ESC" button is pressed it will freeze the game and a panel with 3 buttons will appear and if is pressed again it will unfreeze the game.
    Resume button will unfreeze the game
    Menu button will go to "MainMenu" scene
    Quit button will close the game but if is played in unity it will do nothing

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    private string loadScene = "MainMenu";
+    public GameObject pauseMenuUi;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused) {
+                ResumeGame();
+            }
+            else{
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame(){
+        pauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void PauseGame() {
+        pauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(loadScene);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+}
